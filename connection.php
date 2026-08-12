05eed9e8bd28cf5483071ee4f86ed53c77b189b0
Amos M. Abdallah <amgenious28@gmail.com>
2026-08-12 16:18:48 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,12 +2,13 @@
 
 
 
+
 $host = getenv('DB_HOST') ?: 'localhost';
 $user = getenv('DB_USER') ?: 'root';
 $password = getenv('DB_PASSWORD') ?: '';
 $dbname = getenv('DB_NAME') ?: 'mafiga';
 $port = (int) (getenv('DB_PORT') ?: 3306);
 
-$conn = new mysqli($server, $username, $password, $db);
+$conn = new mysqli($host, $user, $password, $dbname, $port);
 
 ?>
