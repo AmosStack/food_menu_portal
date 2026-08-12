ab9cba3b941f46dd8927ec2c91daf1dbeca8d7e5
Amos M. Abdallah <amgenious28@gmail.com>
2026-08-12 16:27:53 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -11,4 +11,9 @@ $port = (int) (getenv('DB_PORT') ?: 3306);
 
 $conn = new mysqli($host, $user, $password, $dbname, $port);
 
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
 ?>
