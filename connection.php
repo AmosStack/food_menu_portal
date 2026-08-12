98521bcc8e34800a9437dd6d64571ec49fcf603a
Amos M. Abdallah <amgenious28@gmail.com>
2026-08-12 16:15:51 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,12 @@
 <?php
 
-$server = "localhost";
-$username = "root";
-$password = "";
-$db = "food_menu_db";
+
+
+$host = getenv('DB_HOST') ?: 'localhost';
+$user = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASSWORD') ?: '';
+$dbname = getenv('DB_NAME') ?: 'mafiga';
+$port = (int) (getenv('DB_PORT') ?: 3306);
 
 $conn = new mysqli($server, $username, $password, $db);
 
